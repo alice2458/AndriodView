f8da766ff30355f826edbc5e6cc1dd4475b45082
Alice2458 <18845788065@163.com>
2018-07-19 14:44:37 +0800

    lpp

--- app/src/main/java/com/example/alice/view/MainActivity.java
+++ app/src/main/java/com/example/alice/view/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.alice.view;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private TimePicker myTp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myTp = (TimePicker) findViewById(R.id.tp1);
+        myTp.setIs24HourView(true);
+        myTp.setCurrentHour(18);
+        myTp.setCurrentMinute(30);
     }
 }
